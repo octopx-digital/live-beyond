381b8f918e613d7ab0d81046c6a1049f7dfef817
Barbara Bombachini <bbombachini@gmail.com>
2018-03-03 15:08:47 -0500

    add config file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var handlebars = require('handlebars');
-var exphbs  = require('express-handlebars');
+var xhb  = require('express-handlebars');
 
 // var xhb = require('ejs');
 
@@ -16,8 +16,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.engine('ejs', xhb({defaultLayout: 'main'}));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', xhb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 // app.set('view engine', 'ejs');
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Live Beyond Your Life' 
+  res.render('home', {
+    title: 'Live Beyond Your Life'
   });
 });
 
