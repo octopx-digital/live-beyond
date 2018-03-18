514d77c7c749ee78b01c87ed861cd75fe7f64e75
Barbara Bombachini <bbombachini@gmail.com>
2018-03-18 01:21:25 -0400

    Add function to stop gallery on the last item

--- public/js/modules/event_gallery.js
+++ public/js/modules/event_gallery.js
@@ -1,11 +1,17 @@
 export default function eventArrows(){
+  console.log('called arrows');
   let toLeft = document.querySelector('.nav-back');
   let toRight = document.querySelector('.nav-forward');
   let events = document.querySelector('#events-container');
   let thumbs = events.children.length;
   let thumbcount = events.querySelectorAll('.events');
   let thumb = thumbcount[0].offsetWidth;
+  console.log('thumb '+thumb);
   let thumbWidth = events.offsetWidth;
+  // let rightPos = events.getBoundingClientRect();
+  // console.log(rightPos);
+  var holder = document.querySelector('.events-holder').getBoundingClientRect().right;
+  // console.log("holder right:"+holder);
   let leftPosition = 0;
   events.style.left = leftPosition+"px";
 
@@ -30,6 +36,13 @@ export default function eventArrows(){
 
   function moveForward(){
     console.log('forward');
+    let rightPos = events.getBoundingClientRect().right;
+    if (window.innerWidth > 640){
+      if(Math.ceil(rightPos) <= Math.floor(holder + thumb)){
+        console.log('equal');
+        return;
+      }
+    }
     if (leftPosition > (thumbs-1) * -thumb) {
       moveSlide(-1);
     } else {
